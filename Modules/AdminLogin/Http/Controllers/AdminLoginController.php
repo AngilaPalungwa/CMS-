898d0c9b75b7df309c27b03b2abf7322e4f5ec91
AngilaPalungwaLimbu <nzelapalungwa03@gmail.com>
2023-12-24 11:47:21 +0545

    login

---
+++ Modules/AdminLogin/Http/Controllers/AdminLoginController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Modules\AdminLogin\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AdminLoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Renderable
+     */
+    public function index()
+    {
+        return view('adminlogin::login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Renderable
+     */
+    public function submit(Request $request)
+    {
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required|min:6',
+        ]);
+        $email=$request->email;
+        $password=$request->password;
+        $user=User::where('email',$email)->first();
+        dd($user);
+        if($user){
+
+            if($user->password){
+                if(Hash::check($password, $user->password))
+                {
+                    Auth::guard('admin')->check();
+                    Auth::guard('admin')->login($user);
+                    return redirect()->route('admin.dashboard');
+                }
+                $request->session()->flash('error','Password doesnt match');
+
+            }else{
+                $request->session()->flash('error','Password doesnt match');
+                return redirect()->back();
+            }
+        }
+        $request->session()->flash('error','User Not Found');
+        return redirect()->back();
+        // return view('adminlogin::submit');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Renderable
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function show($id)
+    {
+        return view('adminlogin::show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param int $id
+     * @return Renderable
+     */
+    public function edit($id)
+    {
+        return view('adminlogin::edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Renderable
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Renderable
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data=[
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>'123456',
+            'username'=>'admin',
+            'mobile'=>'9819031881',
+        ];
+        DB::table('users')->insert($data);
+    }
+}
